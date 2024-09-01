5f92432780cd53acfd0627057aabf52b8ea71746
Gustavo <gustavooliveiracunha812@gmail.com>
2024-08-31 23:59:34 -0300

    docs: add the rest of the documentation for the domain layer

--- Domain/Interfaces/IUnitOfWork.cs
+++ Domain/Interfaces/IUnitOfWork.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace CleanArchitecture.Domain.Interfaces
 {
+    /// <summary>
+    /// Represents the Unit of Work pattern for coordinating the writing of changes to the database.
+    /// </summary>
     public interface IUnitOfWork
     {
+        /// <summary>
+        /// Commits all changes made in the current transaction.
+        /// </summary>
+        /// <param name="cancellationToken">Token to cancel the operation.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
         Task Commit(CancellationToken cancellationToken);
     }
 }
--- Domain/Interfaces/IUserRepository.cs
+++ Domain/Interfaces/IUserRepository.cs
@@ -4,11 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace CleanArchitecture.Domain.Interfaces
 {
-    public interface IUserRepository :  IBaseRepository<User>
+    /// <summary>
+    /// Represents the repository interface for managing user entity.
+    /// </summary>
+    public interface IUserRepository : IBaseRepository<User>
     {
+        /// <summary>
+        /// Retrieves a user entity by its email address.
+        /// </summary>
+        /// <param name="email">The email address of the user.</param>
+        /// <param name="cancellationToken">Token to cancel the operation.</param>
+        /// <returns>Returns the user entity if found; otherwise, <c>null</c>.</returns>
         Task<User> GetByEmail(string email, CancellationToken cancellationToken);
     }
 }
