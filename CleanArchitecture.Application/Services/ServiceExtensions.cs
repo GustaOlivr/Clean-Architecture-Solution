bdd0ae3da20ac930cca74235184091273db6d723
Gustavo <gustavooliveiracunha812@gmail.com>
2024-09-01 13:16:54 -0300

    feat: add configuration of important application services

---
+++ CleanArchitecture.Application/Services/ServiceExtensions.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace CleanArchitecture.Application.Services
+{
+    /// <summary>
+    /// Provides extension methods for configuring application-level services.
+    /// </summary>
+    public static class ServiceExtensions
+    {
+        /// <summary>
+        /// Configures application services including AutoMapper, MediatR, and FluentValidation.
+        /// </summary>
+        /// <param name="services">The service collection to add the services to.</param>
+        public static void ConfigureApplicationApp(this IServiceCollection services)
+        {
+            // Registers AutoMapper with the assemblies of the current executing application.
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            // Registers MediatR with the assemblies of the current executing application.
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+            // Registers all FluentValidation validators from the assemblies of the current executing application.
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
