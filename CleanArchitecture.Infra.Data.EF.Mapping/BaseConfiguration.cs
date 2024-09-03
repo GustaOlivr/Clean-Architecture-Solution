bba0b3efc51b20c86288e3d9a49acefc9dad8bf2
Gustavo <gustavooliveiracunha812@gmail.com>
2024-09-03 10:54:46 -0300

    feat: add EF mapping configuration

---
+++ CleanArchitecture.Infra.Data.EF.Mapping/BaseConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchitecture.Infra.Data.EF.Mapping
+{
+    public abstract class BaseEntityConfiguration<T> : IEntityTypeConfiguration<T> where T : class
+    {
+        public virtual void Configure(EntityTypeBuilder<T> builder)
+        {
+            
+        }
+    }
+}
---
+++ CleanArchitecture.Infra.Data.EF.Mapping/User/UserConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Infra.Data.EF.Mapping.User
+{
+    public sealed class UserConfiguration : BaseEntityConfiguration<Domain.Entities.User>
+    {
+        public override void Configure(EntityTypeBuilder<Domain.Entities.User> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
+            builder.Property(x => x.Email).HasMaxLength(50).IsRequired();
+
+            base.Configure(builder);
+        }
+    }
+}
