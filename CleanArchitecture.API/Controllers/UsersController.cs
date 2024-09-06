6b66e0fce8a9b23231ec736aa0cb99f4f8333f35
GtOliv3r <123522878+GtOliv3r@users.noreply.github.com>
2024-09-06 12:09:21 -0300

    fix: fix the GetAll endpoint

--- CleanArchitecture.API/Controllers/UsersController.cs
+++ CleanArchitecture.API/Controllers/UsersController.cs
@@ -57,7 +57,7 @@ namespace CleanArchitecture.API.Controllers
         [HttpGet]
         public async Task<ActionResult<List<GetAllUserResponse>>> GetAll(CancellationToken cancellationToken)
         {
-            var response = await _mediator.Send(new GetAllUserResponse(), cancellationToken);
+            var response = await _mediator.Send(new GetAllUserRequest(), cancellationToken);
             return Ok(response);
         }
     }
